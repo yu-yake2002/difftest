1df6c49b79513209b0fc4735a5891312293e3391
yu-yake2002 <yuyake02@outlook.com>
2025-05-16 12:55:23 +0800

    feat(matrix): Adds AmuCtrl event and AME CSRs support to Difftest framework
    - Introduces AmuCtrl event handling, including new classes and methods for event recording, queuing,
      and validation.
    - Add support for CSRs in AME.

--- src/test/csrc/difftest/difftest.h
+++ src/test/csrc/difftest/difftest.h
@@ -389,6 +389,11 @@ protected:
   void cmo_inval_event_record();
 #endif
 
+#ifdef CONFIG_DIFFTEST_AMUCTRLEVENT
+  std::queue<DifftestAmuCtrlEvent> amu_ctrl_event_queue;
+  void amu_ctrl_event_record();
+#endif
+
   void update_last_commit() {
     last_commit = get_trap_event()->cycleCnt;
   }
@@ -409,6 +414,7 @@ protected:
   int do_ptwrefill_check();
   int do_l1tlb_check();
   int do_l2tlb_check();
+  int do_amuctrl_check();
   int do_golden_memory_update();
 
   inline uint64_t get_commit_data(int i) {
